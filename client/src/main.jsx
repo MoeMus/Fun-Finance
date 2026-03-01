ed9b0ce7b028ae857ab81efa04cffef60065a487
MoeMus <144395260+MoeMus@users.noreply.github.com>
2026-02-28 18:50:03 -0800

    Add new dashboard with dragon functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import redux_store from "./redux_store";
-import {BrowserRouter} from "react-router";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
