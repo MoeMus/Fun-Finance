6fbc84f8f52d55bb03050bb06f5d18101a5f1c94
JamesChuong <jcjameschuong@gmail.com>
2026-02-28 16:47:40 -0800

    Fix merge conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,16 @@ import './App.css';
 import { Route, Routes } from "react-router";
 import LoginPage from "./authentication/Login.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { useSelector } from 'react-redux'
 
 function App() {
+
+  let { is_logged_in } = useSelector((state) => state.authTokenSlice)
+
   return (
     <div className="app-main">
       <Routes>
-        <Route path={"/"} element={ <ProtectedRoute protected_component={ <PredictiveDashboard /> } isLoggedIn={false} /> } />
+        <Route path={"/"} element={ <ProtectedRoute protected_component={ <PredictiveDashboard /> } isLoggedIn={is_logged_in} /> } />
         <Route path={"/login"} element={ <LoginPage /> } />
       </Routes>
     </div>
--- client/src/auth_token_store/auth_token_slice.js
+++ client/src/auth_token_store/auth_token_slice.js
@@ -3,19 +3,19 @@ import { createSlice } from '@reduxjs/toolkit'
 const authTokenSlice = createSlice({
   name: 'AuthenticationSlice',
   initialState: {
-    authToken: sessionStorage.getItem('access_token') || null,
-    isLoggedIn: !!sessionStorage.getItem('access_token')
+    access_token: sessionStorage.getItem('access_token') || null,
+    is_logged_in: !!sessionStorage.getItem('access_token')
   },
   reducers: {
     setAuthToken: (state, action) => {
       sessionStorage.setItem('access_token', action.payload.access_token);
-      state.isLoggedIn = true;
-      state.authToken = action.payload.access_token
+      state.is_logged_in = true;
+      state.access_token = action.payload.access_token
     },
     removeAuthToken: (state, action) => {
       sessionStorage.removeItem('access_token');
-      state.isLoggedIn = false;
-      state.authToken = null;
+      state.is_logged_in = false;
+      state.access_token = null;
     }
   }
 });
